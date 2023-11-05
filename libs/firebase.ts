d26a2c2e0f842b58c53e3c58bf488f6b04c4d05b
NeelClaudel <neel.claudel@gmail.com>
2023-11-05 03:07:04 +0100

    firebase+mongo

--- libs/firebase.ts
+++ libs/firebase.ts
@@ -2,12 +2,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyABC6y4dVJXFYyJ194SWNwcvb-kI2Bu9hg",
+  authDomain: "woolies-b0f59.firebaseapp.com",
+  projectId: "woolies-b0f59",
+  storageBucket: "woolies-b0f59.appspot.com",
+  messagingSenderId: "887722769967",
+  appId: "1:887722769967:web:1410ff6fb704df04a750da",
+  measurementId: "G-DHFSP5K5VB",
 };
 
 // Initialize Firebase
